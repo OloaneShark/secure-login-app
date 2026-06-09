de0bd22b36adfbee7be20decf53f8971cf8bbfc8
OloaneShark <oloanedennis0@gmail.com>
2026-06-09 13:53:23 -0400

    Added user and password to app.py and updated login.html

--- app.py
+++ app.py
@@ -6,8 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/login")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        print(f"Username: {username}")
+        print(f"Password: {password}")
+
     return render_template("login.html")
 
 if __name__ == "__main__":
