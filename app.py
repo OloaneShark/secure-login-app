dd5eaf7e0e4fccbcd67f0ed73ca7991402eae995
OloaneShark <oloanedennis0@gmail.com>
2026-06-09 14:02:02 -0400

    Created register.html and added route(register) to app.py and updated index.html

--- app.py
+++ app.py
@@ -27,5 +27,18 @@ def login():
 
     return render_template("login.html")
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
+
+        print(f"Register Username: {username}")
+        print(f"Register Password: {password}")
+        print(f"Confirm Password: {confirm_password}")
+
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
