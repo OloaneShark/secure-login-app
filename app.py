f079b0f674d1b16aed3357860d1dff7992fb261e
OloaneShark <oloanedennis0@gmail.com>
2026-06-10 10:39:28 -0400

    Created seperate thing file for models

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from models import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
@@ -10,13 +11,7 @@ app.secret_key = "supersecretkey"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+db.init_app(app)
 
 
 @app.route("/")
